3b315038274e787987b2dc3c1a091345ece20895
Emily Joanna Alves <88635006+emilyalvese@users.noreply.github.com>
2023-08-07 21:45:31 -0300

    Update MainForma.java

--- Lista1PDM/src/ex3/MainForma.java
+++ Lista1PDM/src/ex3/MainForma.java
@@ -3,7 +3,16 @@ package ex3;
 public class MainForma {
 
 	public static void main(String[] args) {
-		
+		Forma f1 = new Retangulo(5, 10);
+		Forma f2 = new Quadrado(7);
+		Forma f3 = new Circulo(4);
+
+		System.out.println("Área do Retângulo: " + f1.area());
+		System.out.println("Perímetro do Retângulo: " + f1.perimetro());
+		System.out.println("Área do Quadrado: " + f2.area());
+		System.out.println("Perímetro do Quadrado: " + f2.perimetro());
+		System.out.println("Área do Círculo: " + f3.area());
+		System.out.println("Perímetro do Círculo: " + f3.perimetro());
 	}
 
 }
