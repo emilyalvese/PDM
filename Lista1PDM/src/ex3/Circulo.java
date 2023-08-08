8713b26429824d9e996a858e8a56d38d45ef96ac
Emily Joanna Alves <88635006+emilyalvese@users.noreply.github.com>
2023-08-07 21:42:18 -0300

    Update Circulo.java

--- Lista1PDM/src/ex3/Circulo.java
+++ Lista1PDM/src/ex3/Circulo.java
@@ -2,4 +2,18 @@ package ex3;
 
 public class Circulo extends Forma {
 
+  private double raio;
+
+  public Circulo(double raio) {
+       this.raio = raio;
+  }
+
+  public double area() {
+      return Math.PI*Math.pow(raio, 2);
+  }
+
+  public double perimetro() {
+     return 2*Math.PI*raio;
+  }
+ 
 }
